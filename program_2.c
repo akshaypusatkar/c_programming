afa86c0daed60a624b99f56287a6f068940fc93c
akshaypusatkar <39702891+akshaypusatkar@users.noreply.github.com>
2018-06-03 01:03:41 +0530

    Update program_2.c

--- program_2.c
+++ program_2.c
@@ -1,3 +1,6 @@
+/*The distance between two cities (in km) is input through the keyboard. 
+WAP to convert and print this distance in meters,feet,inches and centimeters.*/ 
+
 #include<stdio.h>
 int main()
 { 
