39ed67a229fae61c35a96f58529604630207a8bc
akshaypusatkar <39702891+akshaypusatkar@users.noreply.github.com>
2018-06-03 00:54:28 +0530

    Add files via upload

---
+++ program_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+int basic_salary,dearness_allowance,house_rent,gross_salary;
+printf("Enter Ramesh's basic salary = ");
+scanf("%d",&basic_salary);
+dearness_allowance = (basic_salary*40)/100;
+house_rent = (basic_salary*20)/100;
+gross_salary = (basic_salary)+(dearness_allowance)+(house_rent);
+printf("Ramesh's Dearness Allowance = %d\n",dearness_allowance);
+printf("Ramesh's house rent = %d\n",house_rent);
+printf("Ramesh's Gross Salary = %d\n",gross_salary);
+return 0;
+}
